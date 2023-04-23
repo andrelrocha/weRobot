35d60b68f9c8898d63aca77d5ca38dd240ec64e8
andrelrocha <andrerocha0911@gmail.com>
2023-04-23 15:40:06 -0300

    fix: consertando bug de responsividade no width do menu scroll

--- public/script/mobile.js
+++ public/script/mobile.js
@@ -1,8 +1,10 @@
 function scrollMenu (event) {
     const homeNav = document.querySelector('#navigation');
    
-    if (scrollY > 1) {
-        homeNav.classList.add('scroll')
+    if (scrollY > 0) {
+        homeNav.classList.add('scroll');
+        // define width como o tamanho atual da janela de exibição, garantindo que ele tenha uma largura fixa independente do tamanho da tela.
+        homeNav.style.width = window.innerWidth + "px";
     } else {
         homeNav.classList.remove('scroll')
     }
